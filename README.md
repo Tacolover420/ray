b384d887fd411ebe4d35a0a5e10d40f63219fc3c
Nychtgian <nichtgian@gmail.com>
2018-01-22 13:30:52 +0100

    -

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Raycasting
-### redone with not three.js and more swag
-https://nichtgian.github.io/ray/
+## redone with more swag
+
+[Play It](https://nichtgian.github.io/ray/)
+
+## Future included / planned Features:
+
+- Multiple Gamemodes (Prop Hunt, Survival, Bomberman,  Arena Shooter..)
+- Lobby to join Servers, see other players
+- Charakter / Weapon customisation
+- Host private Lobbys / Gamemodes
+- Friends / Crew system
+
+
+## Features
+
+- Raycasting Engine in JavaScript
+- Renders textured Walls and Sprites (2D Objects)
+
+> Project is still in early development!
--- script/system.js
+++ script/system.js
@@ -2,3 +2,19 @@
 let canvas = document.getElementById("display");
 let ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
+
+Texture.Material.Sky.src = "texture/sky.jpg";
+Texture.Wall.Stone.Load.src = "texture/wall/stone.jpg";
+Texture.Wall.Wood.Load.src = "texture/wall/wood.jpg";
+Texture.Wall.Brick.Load.src = "texture/wall/brick.jpg";
+Texture.Wall.Cobble.Load.src = "texture/wall/cobble.png";
+Texture.Wall.Colorstone.Load.src = "texture/wall/colorstone.png";
+Texture.Wall.Purplestone.Load.src = "texture/wall/purplestone.png";
+Texture.Wall.Bluestone.Load.src = "texture/wall/bluestone.png";
+Texture.Wall.Mossy.Load.src = "texture/wall/mossy.png";
+Texture.Sprite.Table.Load.src = "texture/sprite/table.png";
+Texture.Sprite.Armor.Load.src = "texture/sprite/armor.png";
+Texture.Sprite.Light.Load.src = "texture/sprite/light.png";
+Texture.Sprite.Plant.Load.src = "texture/sprite/plant.png";
+Texture.Sprite.Barrel.Load.src = "texture/sprite/barrel.png";
+Texture.Sprite.Pillar.Load.src = "texture/sprite/pillar.png";
